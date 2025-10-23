6a199a23edbb52477f53d6d865b2fed099dbcbde
mohamedfekryhui <mesofekry123456789@gmail.com>
2025-10-23 23:32:01 +0300

    d

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5325673701011280"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
